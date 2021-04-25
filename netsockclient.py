c4ea5fe882a089251db5d38a6801c25f009a4635
kali <kali@kalim1.localdomain>
2021-04-24 21:26:11 -0400

    modifying server

--- netsockclient.py
+++ netsockclient.py
@@ -9,4 +9,14 @@ print("Connection established with server")
 
 mess = input("Type message to send to server: ")
 client_s.sendall(mess.encode())
+
+# Look for the response
+amount_received = 0
+amount_expected = len(mess)
+
+while amount_received < amount_expected:
+    data = client_s.recv(16)
+    amount_received += len(data)
+    print(data)
+
 client_s.close()
--- netsockserv.py
+++ netsockserv.py
@@ -1,7 +1,8 @@
 #Python code for socket practice
 
-#importing socket module
+#importing socket and sys modules
 import socket
+import sys
 
 #Setting host and port variables for server based on user input
 host = input ("Type the server IP address: ")
@@ -10,26 +11,33 @@ port = int(input("Type the server port #: "))
 #Create socket based on inet4 family address and TCP connection
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Bind socket to user specified host addr and port num
-s.bind((host, port))
+try:
+    #Bind socket to user specified host addr and port num
+    s.bind((host, port))
+# Return error message if bind fails, and exit
+except socket.error as msg:
+    print('Bind failed')
+    sys.exit()
+
+#Indicate successful bind of socket 
+print("# Socket bind complete")
 
 #Setting the number of queued connections to the socket listening for a connection
-s.listen(1)
-
-#Output to let the user know the socket server is awaiting a connection
-print("Server has started; awaiting connection...")
+s.listen(3)
+print("# Socket now listening")
 
 #Accepting incoming connections, returning the socket object (conect) and the client address bound to the socket (addr)
 connect, addr = s.accept()
 
 #Print the info describing how the client is connected to the server socket
-print("Connected at ", addr)
+print('# Connected to ' + addr[0] + ':' + str(addr[1]))
 
 #Iterate over the data sent between the server and client, and continue to print as long as there is data being transmitted
 #When no data exists, close the connection
-while 1:
+while True:     
     data = connect.recv(1024)
-    if not data: break
-    connect.sendall(b'<---Message--->\n')
-    print(data.decode('utf-8'))
-connect.close()
+    line = data.decode('UTF-8')    # convert to string
+    line = line.replace("\n","")   # remove newline character
+    print(line)
+
+s.close()
